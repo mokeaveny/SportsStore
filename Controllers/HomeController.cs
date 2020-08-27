a1cc9d8795b1cebbc44089bf69d62f97944cf6e5
Michael Keaveny <michaelkeaveny0@gmail.com>
2020-08-27 20:34:53 +0100

    Created a database for products and the controller and view for the products.#2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index() => View(repository.Products);
+    }
+}
