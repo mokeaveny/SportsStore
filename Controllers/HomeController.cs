a57b54eed0f4eeb13b4bbd08436e0d87664bdcb0
Michael Keaveny <michaelkeaveny0@gmail.com>
2020-08-28 11:56:51 +0100

    Added pagination with tag helpers.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 using System.Linq;
 
 namespace SportsStore.Controllers
@@ -16,9 +17,19 @@ namespace SportsStore.Controllers
         }
 
         public ViewResult Index(int productPage = 1)
-            => View(repository.Products
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
                 .OrderBy(p => p.ProductID)
                 .Skip((productPage - 1) * PageSize)
-                .Take(PageSize));
+                .Take(PageSize),
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
